69042ee29915f150d6977c5021c4effd3ddeb54e
José Carvajal <jose.carvajal@edify.cr>
2022-07-08 11:02:53 -0600

    feat(app): learn about property and event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'basics';
+  name = 'Jose';
+  imgURL = 'https://picsum.photos/id/237/500/500';
+
+  getName() {
+    return this.name;
+  };
+
+  changeImage(e: KeyboardEvent) {
+    this.imgURL = (e.target as HTMLInputElement).value;
+  };
 }
